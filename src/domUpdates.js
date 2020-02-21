b0d7fd045f8b0b4859c6aae1f37f37c04f7ef83b
“Greg_Grimes” <cggrimes@yahoo.com>
2020-02-21 10:48:18 -0700

    Set color variables and nest some elements in the welcome banner

--- src/domUpdates.js
+++ src/domUpdates.js
@@ -2,7 +2,7 @@ import $ from 'jquery'
 
 const domUpdates = {
 
-  welcomeMessage: function(firstName) {
+  welcomeMessage: (firstName) => {
     $(".banner-image").prepend(`
       <div class="welcome-msg">
         <h1>Welcome ${firstName}!</h1>
@@ -10,7 +10,7 @@ const domUpdates = {
     `)
   },
 
-  makeCard: function(recipeInfo, shortRecipeName) {
+  makeCard: (recipeInfo, shortRecipeName) => {
     $('main').append(`
       <div class="recipe-card" id=${recipeInfo.id}>
         <h3 maxlength="40">${shortRecipeName}</h3>
@@ -26,21 +26,21 @@ const domUpdates = {
     `)
   },
 
-  listTag: function(cap, tag) {
+  listTag: (cap, tag) => {
     $(".tag-list").append(`
       <li><input type="checkbox" class="checked-tag" id="${tag}">
       <label for="${tag}">${cap}</label></li>
     `)
   },
 
-  displayPantryInfo: function(ingredient) {
+  displayPantryInfo: (ingredient) => {
     $(".pantry-list").append(`
       <li><input type="checkbox" class="pantry-checkbox" id="${ingredient.name}">
       <label for="${ingredient.name}">${ingredient.name}, ${ingredient.count}</label></li>
     `)
   },
 
-  makeRecipeTitle: function(recipe, ingredients) {
+  makeRecipeTitle: (recipe, ingredients) => {
     $(".recipe-instructions").append(`
       <button id="exit-recipe-btn">X</button>
       <h3 id="recipe-title">${recipe.name}</h3>
