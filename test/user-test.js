573ec903a9ad02275fac93f8c662460d6e049a9e
“Greg_Grimes” <cggrimes@yahoo.com>
2020-02-25 11:04:20 -0700

    Add tests and note issues in test file

--- test/user-test.js
+++ test/user-test.js
@@ -1,3 +1,10 @@
+// ***ISSUES***
+//The search functionality is currently case sensitive
+//Users should be able to search through filtered results, and filter
+//  through searched resulted. Can use a conditional to check if the
+//  other array if null before running
+//Need to call search/filter methods in scripts, or set conditionals for those
+//  functions to only run when on a certain page
 import { expect } from 'chai';
 
 import User from '../src/user';
@@ -12,7 +19,7 @@ describe('User', function() {
     userInfo = users[0];
     user = new User(userInfo)
 
-    recipe = {name: 'Chicken Parm', type: ['italian', 'dinner']};
+    recipe = {name: 'Chicken Parm', type: ['italian', 'dinner'], ingredients: ['noodles', 'tomatoes']};
   });
 
   it('should be a function', function() {
@@ -22,7 +29,7 @@ describe('User', function() {
   it('should initialize with an id', function() {
     expect(user.id).to.eq(1);
   });
-
+/////////// DEFAULT VALUES
   it('should initialize with a name', function() {
     expect(user.name).to.eq('Rocky Padberg');
   });
@@ -39,9 +46,43 @@ describe('User', function() {
     expect(user.recipesToCook).to.deep.equal([]);
   });
 
+  it('should initialize with a property named filteredFavorites assigned a value of null', function() {
+    expect(user.filteredFavorites).to.equal(null);
+  });
+
+  it('should initialize with a property named searchedRecipes assigned a value of null', function() {
+    expect(user.filteredFavorites).to.equal(null);
+  });
+///////////////////
+
+/////////// METHODS
   it('should be able to save a recipe to favoriteRecipes', function() {
     user.favoriteRecipe(recipe);
     expect(user.favoriteRecipes[0].name).to.equal('Chicken Parm');
+  });
+
+  it('should be able to remove a recipe from favoriteRecipes', function() {
+    user.favoriteRecipe(recipe);
+    user.removeRecipe(recipe);
+    expect(user.favoriteRecipes).to.deep.equal([]);
+  });
+
+  it('should be able to filter favoriteRecipes by type', function() {
+    user.favoriteRecipe(recipe);
+    user.filterFavoriteRecipes('italian');
+    expect(user.filteredFavorites).to.deep.equal([recipe]);
+  });
+
+  it('should be able to search favoriteRecipes by recipe name', function() {
+    user.favoriteRecipe(recipe);
+    user.searchFavoriteRecipes('Chicken');
+    expect(user.searchedRecipes).to.deep.equal([recipe]);
+  });
+
+  it('should be able to search favoriteRecipes by ingredient', function() {
+    user.favoriteRecipe(recipe);
+    user.searchFavoriteRecipes('noodles');
+    expect(user.searchedRecipes).to.deep.equal([recipe]);
   });
 
   it('should be able to decide to cook a recipe', function() {
@@ -49,13 +90,11 @@ describe('User', function() {
     expect(user.recipesToCook[0].name).to.equal('Chicken Parm');
   });
 
-  // it('should be able to filter recipes by type', function() {
-  //   user.saveRecipe(recipe);
-  //   expect(user.filterRecipes('italian')).to.deep.equal([recipe]);
-  // });
+  it('should be able to remove a recipe it decided to cook', function() {
+    user.decideToCook(recipe);
+    user.removeRecipeToCook(recipe);
+    expect(user.recipesToCook).to.deep.equal([]);
+  });
+  ///////////////////
 
-  // it('should be able to search recipes by name', function() {
-  //   user.saveRecipe(recipe);
-  //   expect(user.searchForRecipe('Chicken Parm')).to.deep.equal([recipe]);
-  // });
 });
